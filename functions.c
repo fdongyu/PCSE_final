58d244dad9b33b63f45598ec30b8d52b5c054e7d
冯东雨 <fengdongyu@fengdongyus-MacBook-Pro.local>
2019-05-06 22:57:07 -0500

    add float type to function CalculateDistance

--- functions.c
+++ functions.c
@@ -57,7 +57,7 @@ void init_random(float x0, float y0, int Npx, int Npy, float *xinit, float *yini
 
 
 
-float CalculateDistance(x1, y1, x2, y2){
+float CalculateDistance(float x1, float y1, float x2, float y2){
     return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 }
 
